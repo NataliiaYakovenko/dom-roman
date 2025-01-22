8ff6952b1acd0576f31bd54e0e4a5a85c902f583
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-22 22:51:19 +0200

    DOM приклад з unset

--- index.js
+++ index.js
@@ -9,22 +9,31 @@ console.dir(document);
  */
 //1.Зареєструвати обробник події натискання миші на кнопку
 //2. Навісили обробник натискання миші накнопку
-function getAlert(){
+function getAlert(event){
     alert('Hello user')
+    console.log(event);
 }
 const btn = document.getElementById('btn')
 console.log(btn);
 btn.addEventListener('click', getAlert)
+
 //-------------------------------------------------
 /*
 Зробіть кнопку, додайте id
 Напишіть функцію, яка виводит в консоль Привіт
 Навісити обробку події наведення миші на цю кнопку
 */
+
+function getGreeting(event){
+    const but = event.target;
+    
+    if(but.style.backgroundColor === 'red'){
+        but.style.backgroundColor = 'unset';
+    }else{
+        but.style.backgroundColor = 'red';
+        
+    }
+}
 const button = document.getElementById('button');
 console.log(button);
-
-function getGreeting(){
-console.log('Hello');
-}
 button.addEventListener('mouseenter',getGreeting)
