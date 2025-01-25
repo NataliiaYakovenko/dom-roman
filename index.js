bc84d20352cade3abfb10627d5d714693a14c99b
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-25 08:07:04 +0200

    Add Конвектор валют

--- index.js
+++ index.js
@@ -1,19 +1,54 @@
-const btn = document.querySelector('button');
+/*
+Створити конвертер валют
 
-const btn2 = document.querySelector('.super-btn');
+Декомпозиція - це вирішення задачі частинами:
+1.Маємо створити HTML сторінку з формою, яка містить два поля вводу
+ - одне поле для ведення суми в одній валюті
+ - інше поле - для вибору валюти, в яку ми будемо цю суму конвертувати
+ 
+2. Додати в форму кнопку 'Конвертувати' 
 
-const btn3 = document.querySelector('#btnbtn');
+3. Написати JS обробник події відправки форми
+ - отримати значення введеної суми та обраної валюти
+ - обчислити конвертовану суму, помноживши введену користувачем суму
+ на курс обміну вибраної користувачем валюти
+ - вивести результат конвертації на сторінку для відображеня результату (textContent)
 
-const btnCollection = document.querySelectorAll('button');
+4. Підписати форму на подію відправки форми 'submit', 
+в якості слухача використовувати функцію з пункта 3  
 
-const paragraphCollection = document.querySelectorAll('.paragraph:nth-child(even)')
-paragraphCollection[0].textContent = 'Nataliia is beautiful girl'
-
-for(let p of paragraphCollection){
-    p.style.color = 'blue';
-    p.style.backgroundColor = 'yellow';
+*/
+//-----------------------------------------------------------------
+//створюємо об'єкт для зберігання курсу валют
+const CURRENCY = {
+  USD_TO_UAH: 37.65,
+  EUR_TO_UAH: 40.61
 }
-console.log(paragraphCollection);
+const convertorForm = document.querySelector('#currency-converter-form');
 
 
+function convertHandler(event){
+    event.preventDefault() // метод, який зупинить перезавантаження сторінки
+  
+    //отримуємо значення введеної суми та обраної валюти
+    //число повертається  string, тому використовуємо конструктор Number
+    const amount = Number(document.querySelector('#amount').value);
+    const currency = document.querySelector('#currency').value;
+    
+    let convertAmount; // сюди покладемо конвертовану суму
+    //обчислюємо конвертовану суму
+    if(currency === 'USD'){
+        convertAmount = amount * CURRENCY.USD_TO_UAH //число помножуємо на курс долора
+    } else if(currency === 'EUR'){
+        convertAmount = amount * CURRENCY.EUR_TO_UAH //число помножуємо на курс евро
+    } else{
+        throw new Error('Something problem ...!')
+    }
 
+    //вивводимо результат конвертації на сторінку
+    const result = document.querySelector('#result')
+
+    result.innerHTML = `${amount} ${currency} = ${convertAmount.toFixed(2)} UAH`
+}
+//Підписуємо форму на подію відправки форми 'submit'
+convertorForm.addEventListener('submit', convertHandler);
