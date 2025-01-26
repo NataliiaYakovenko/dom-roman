50a6de34d9b6984700936390291b214322b7242f
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-26 12:12:11 +0200

    Add Data-* Задача кнопки з різними кольорами

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
- 
- const button = document.querySelector('button');
+/*
+Задача
+1. Ми маємо div
+2. Ми маємо 5 кнопок з різними кольорами
+3. За натисненням на кнопку, фоновий колір div 
+має змінитися на відповідний колір, що вказаний на кнопці
+*/
+const button = document.querySelectorAll('button');
+const div = document.querySelector('#root')
 
- button.addEventListener('click',clickHandler);
- function clickHandler({target}){//достучатися тільки до нашої кнопки, на якій відбувається подія
-  console.log(target)
-  target.disabled = true; // після натискання кнопка стане не активною
+//через for перебираємо всі кнопки
+//for(let i = 0; i < button.length; i++){
+  //кожну нашу кнопку підписуємо на подію click
+  //button[i].addEventListener('click',clickHandler); // у кожній кнопці викликаємо обробник
+//}
 
- }
+//або через for перебираємо всі кнопки
+for(let btn of button){
+  btn.addEventListener('click',clickHandler )
+}
+
+function clickHandler({target:{dataset:{color}}}){ //деструктуризуємо target/потім деструктуризуємо dataset
+  //console.dir(dataset); // із target витягуємо dataset
+  //console.dir(color);// із dataset витягуємо color
+  div.style.backgroundColor = color; //color - color, який прихлдить із об'єкту подій
+
+}
