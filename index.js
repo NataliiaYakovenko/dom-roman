3cf1b30136ffb7003a9f7ad626a089bb13d61ca5
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-01-26 15:41:48 +0200

    Add img src Задача dogs

--- index.js
+++ index.js
@@ -1,27 +1,24 @@
 /*
-Задача
-1. Ми маємо div
-2. Ми маємо 5 кнопок з різними кольорами
-3. За натисненням на кнопку, фоновий колір div 
-має змінитися на відповідний колір, що вказаний на кнопці
+https://cdn.e-pet.com.ua/uploaded/2024/10/27/18/IMG_6436_Original-cGUpvY8x.jpg
+
+https://tvoemisto.tv/media/gallery/full/6/8/68822578388_o.jpg
+
+
+В HTML ми маємо елемент картинки img
+
+В HTML ми маємо дві кнопки
+Ці кнопки в дата - атрибутах містять посилання на певну картинку(адреса картинки)
+
+За натисненням на відповідну кнопку має відкритись (відобразитись в тезі img) відповідна цій кнопці картинка
+
 */
-const button = document.querySelectorAll('button');
-const div = document.querySelector('#root')
+const[btn1,btn2] = document.querySelectorAll('button');
 
-//через for перебираємо всі кнопки
-//for(let i = 0; i < button.length; i++){
-  //кожну нашу кнопку підписуємо на подію click
-  //button[i].addEventListener('click',clickHandler); // у кожній кнопці викликаємо обробник
-//}
+btn1.addEventListener('click',clickHandler);
+btn2.addEventListener('click',clickHandler);
 
-//або через for перебираємо всі кнопки
-for(let btn of button){
-  btn.addEventListener('click',clickHandler )
-}
-
-function clickHandler({target:{dataset:{color}}}){ //деструктуризуємо target/потім деструктуризуємо dataset
-  //console.dir(dataset); // із target витягуємо dataset
-  //console.dir(color);// із dataset витягуємо color
-  div.style.backgroundColor = color; //color - color, який прихлдить із об'єкту подій
-
+function clickHandler({target}){
+  const img = target.parentNode.children[0];//наш шлях до картинки
+  console.log(target.dataset.src);// шлях до посилань на картинки
+  img.setAttribute('src',target.dataset.src)
 }
