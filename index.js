4ee93db74cc557f1034f228e9fa24bfe49f30f9c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-03 22:03:46 +0200

    Add game-field варіант2

--- index.js
+++ index.js
@@ -9,14 +9,13 @@
 const gameField = document.querySelector("#game-field");
 const box = document.querySelector("#box");
 
-gameField.addEventListener("click", clickHandler, { capture: true });
+gameField.addEventListener("click", clickHandler);
 
 function clickHandler(event) {
   //Якщо ми натискаємо не ігрове поле(тобто на квадратик) - переміщеню не бути
-  event.stopPropagation();
   //target - це елемент, на якому відбулась подія (на який елемент саме клікнули)
   //currentTarget - якому елементу належав обробник подій
-  
+
   if (event.currentTarget === event.target) {    //Якщо ми натискаємо на ігрове поле - переміщеню бути
     const {target:{children: { box }},clientY,clientX} = event;
 
