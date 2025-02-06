3f28dbf60a1b54f8658932c6ebd30b5d1aa26e3c
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-06 20:56:12 +0200

    Add image wrapper

--- data.js
+++ data.js
@@ -2,50 +2,50 @@ const data = [
     {
         id: 1,
         name:'Nataliia',
-        descroption: 'Descroption for Nataliia',
+        descroption: 'Description for Nataliia',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 2,
         name:'Roland',
-        descroption: 'Descroption for Roland',
-        profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
+        descroption: 'Description for Roland',
+        profilePicture:'httpsll://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 3,
         name:'David',
-        descroption: 'Descroption for David',
+        descroption: 'Description for David',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 4,
         name:'Jeck',
-        descroption: 'Descroption for Jack',
+        descroption: 'Description for Jack',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 5,
         name:'Susan',
-        descroption: 'Descroption for Susan',
+        descroption: 'Description for Susan',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },    
     {
         id: 6,
         name:'Oscar',
-        descroption: 'Descroption for Oscar',
+        descroption: 'Description for Oscar',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 7,
         name:'Lilu',
-        descroption: 'Descroption for Lilu',
+        descroption: 'Description for Lilu',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 6,
         name:'For',
-        descroption: 'Descroption for For',
+        descroption: 'Description for For',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
-    },
+    }
     
 ]
--- index.js
+++ index.js
@@ -1,48 +1,78 @@
 "use strict";
 
-/* <article class="card-wrapper">
-        <img class="card-image" src="https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg" 
-        alt="photo Nataliia">
+/*<article class="card-wrapper">
+        <div class="image-wrapper">
+          <img class="card-image" src="https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg" 
+          alt="photo Nataliia">
+        </div>
          <h2 class="username">Nataliia</h2>
-         <p class="description">Descroption for Nataliia</p>
-      </article> */
+         <p class="description">Description for Nataliia</p>
+      </article>*/
 
 const root = document.querySelector("#root");
 
-function creatUserCard(user) {
-  //Створення img
-  const img = document.createElement("img");
-  img.setAttribute("src", user.profilePicture);
-  img.setAttribute("alt", user.name);
-  img.classList.add("card-image");
+function createUserCard(user) {
+  //Створюємо обгорку для картинки
+  const image = createImageWrapper(user)
   //Створення h2
-  const h2 = creatElement('h2',{clasNames:['username']},user.name)
+  const h2 = createElement("h2", { classNames: ["username"] }, user.name);
   //Створення параграфу
-  const p = creatElement('p',{clasNames:['description']},user.description)
-  //Створюємо і повертаємо article, в який вкладені img, h2, p
-  return creatElement('article',{clasNames:['card-wrapper']},img, h2, p);
+  const p = createElement("p",{ classNames: ["description"] },user.description);
+  //Створюємо і повертаємо article, в який вкладені image, h2, p
+  return createElement("article",{ classNames: ["card-wrapper"] }, image, h2, p);
 }
 
 console.log(data);
-const cardArray = data.map((user) => {
-  return creatUserCard(user);
-});
+const cardArray = data.map((user) => {return createUserCard(user)});
 root.append(...cardArray);
 
-
 /**
- * 
+ *
  * @param {String} type - тег елемента, який нам треба створити
- * @param {String[]} clasNames - список класів, які потрібно додати до елемента
+ * @param {String[]} classNames - список класів, які потрібно додати до елемента
  * @param  {...nodes} childNodes - список дочирніх вузлів
  * @returns {HTMLElement}
  */
-function creatElement(type, { clasNames }, ...childNodes) {
+
+function createElement(type, {classNames}, ...childNodes) {
   //type - назва тега
-  //clasNames - класи об'єктів
+  //classNames - класи об'єктів
   //...children - всі інші
   const elem = document.createElement(type);
-  elem.classList.add(...clasNames);
+  elem.classList.add(...classNames);
   elem.append(...childNodes);
   return elem;
+}
+
+function imageLoadHandler({target}) {
+  console.log("image successfully loaded");
+  const parentWrapper = document.querySelector(`#wrapper${target.dataset.id}`);
+  parentWrapper.append(target)
+}
+
+function imageErrorHandler({target}) {
+  target.remove();
+  console.log("image loading has error");
+}
+
+function createUserImage(user) {
+  const img = document.createElement("img");
+  img.setAttribute("src", user.profilePicture);
+  img.setAttribute("alt", user.name);
+  img.dataset.id = user.id
+  img.classList.add("card-image");
+
+  img.addEventListener("load", imageLoadHandler);
+  img.addEventListener("error", imageErrorHandler);
+  return img;
+}
+
+function createImageWrapper(user) {
+ 
+  //Створення заглушки
+  const image = createElement("div", { classNames: ["image-wrapper"] });
+  image.setAttribute("id",`wrapper${user.id}`);
+  //Створення img
+  const img = createUserImage(user);
+  return image;
 }
