0380ddcefd0fd7bee8b43ef8ad08e9194f9e7bba
Nataliia Yakovenko <yakovenkonatali999@gmail.com>
2025-02-06 22:05:28 +0200

    Add correct image wrapper з фоновою заглушкою

--- data.js
+++ data.js
@@ -2,49 +2,49 @@ const data = [
     {
         id: 1,
         name:'Nataliia',
-        descroption: 'Description for Nataliia',
+        description: 'Description for Nataliia',
         profilePicture:'httpsll://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 2,
         name:'Roland',
-        descroption: 'Description for Roland',
+        description: 'Description for Roland',
         profilePicture:'httpsll://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 3,
         name:'David',
-        descroption: 'Description for David',
+        description: 'Description for David',
         profilePicture:'httpsll://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 4,
         name:'Jeck',
-        descroption: 'Description for Jack',
+        description: 'Description for Jack',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 5,
         name:'Susan',
-        descroption: 'Description for Susan',
+        description: 'Description for Susan',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },    
     {
         id: 6,
         name:'Oscar',
-        descroption: 'Description for Oscar',
+        description: 'Description for Oscar',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
         id: 7,
         name:'Lilu',
-        descroption: 'Description for Lilu',
+        description: 'Description for Lilu',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     },
     {
-        id: 6,
+        id: 8,
         name:'For',
-        descroption: 'Description for For',
+        description: 'Description for For',
         profilePicture:'https://cdn.pixabay.com/photo/2023/06/23/11/23/ai-generated-8083323_640.jpg'
     }
     
